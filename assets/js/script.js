63a54736485f1438da6bb9287b5101d82bca502b
Giamo Lao <giamolao@paradoxicalsw.com>
2020-11-14 06:58:18 -0500

    Update script.js
    added ability to edit tasks/dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,21 +44,24 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+/** When user clicks on task description, convert to <textarea> and allow editing */
 $(".list-group").on("click", "p", function()
 {
+  // Get the current description from the clicked <p>
   var text = $(this)
     .text()
     .trim();
 
+  // Create <textarea> element, and prepare for use to edit task desc
   var textInput = $("<textarea>")
     .addClass("form-control")
     .val(text);
 
+  // Replace the clicked <p> with generated <textarea>
   $(this).replaceWith(textInput);
 
+  // Automatically focus the cursor to the text area
   textInput.trigger("focus");
-
-    console.log(text);
 });
 
 /** revert text input back to <p> on loss of focus */
@@ -74,8 +77,67 @@ $(".list-group").on("blur", "textarea", function() {
     .replace("list-", "");
   // Get the tasks position in the list of other li elements
   let index = $(this)
-    .closet(".list-group-item")
+    .closest(".list-group-item")
     .index();
+
+  // Update the tasks list with the new description
+  tasks[parID][index].text = value;
+  saveTasks();
+
+  // Convert the <textarea> back to a <p> with the updated task description
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(value);
+  $(this).replaceWith(taskP);
+});
+
+// When the user clicks a due date, allow editing of date
+$(".list-group").on("click", "span", function()
+{
+  // get current task date
+  let date = $(this).text().trim();
+
+  // Create new input element
+  let dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // Swap out the <span> for the new <input>
+  $(this).replaceWith(dateInput);
+
+  // Bring element into focus
+  dateInput.trigger("focus");
+});
+
+// When user clicks out of date, save and change back to span
+$(".list-group").on("blur", "input[type='text']", function()
+{
+  // get current text
+  let date = $(this).val().trim();
+
+  // get parent ul's id attribute
+  let parID = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // Get the task's position in the list of other li's
+  let index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // Update task in array and resave to localStorage
+  tasks[parID][index].date = date;
+  saveTasks();
+
+  // recreate span with bootstrap classes
+  let dateSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // Replace <input> with <span> element
+  $(this).replaceWith(dateSpan);
 });
 
 // modal was triggered
